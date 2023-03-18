d6280c556491f778512894c7b210ed5a3d4154a8
Andrey Perevertov <gociioduh@gmail.com>
2023-03-18 09:50:58 +0300

    DZ Seminar9

---
+++ Task66/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int InputData(string text)
+{
+    System.Console.WriteLine(text);
+    int x = Convert.ToInt32(Console.ReadLine());
+    return x;
+}
+
+int OutData(int m, int n)
+{
+    if (m == n+1) return 0;
+    return m + OutData(m+1, n);
+}
+
+int m = InputData("Введите число m: ");
+int n = InputData("Введите число n: ");
+Console.WriteLine(OutData(m, n));
---
+++ Task68/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+int InputData(string text)
+{
+    System.Console.WriteLine(text);
+    int x = Convert.ToInt32(Console.ReadLine());
+    return x;
+}
+
+int AckermanFunctions(int m, int n)
+{
+    if (m == 0) return n + 1;
+    else if (m > 0 && n == 0) return AckermanFunctions(m - 1, 1);
+    else if (m > 0 && n > 0) return AckermanFunctions(m - 1, AckermanFunctions(m, n - 1));
+    else return 0;
+}
+
+int m = InputData("Введите число m: ");
+int n = InputData("Введите число n: ");
+
+if (m >= 0 && n >= 0)
+    Console.WriteLine(AckermanFunctions(m, n));
+else System.Console.WriteLine("Ошибка: Введите не отрицательные целые числа");
